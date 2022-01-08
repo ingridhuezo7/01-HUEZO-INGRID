9893f6b7bb3d644454808af07a0ef89a2df8c7eb
ingridhuezo7 <95658225+ingridhuezo7@users.noreply.github.com>
2022-01-08 13:28:30 -0600

    Add files via upload

--- ANALISIS_02_ HUEZO VAPNIK_INGRID/ANALISIS_02_ HUEZO VAPNIK_INGRID.py
+++ ANALISIS_02_ HUEZO VAPNIK_INGRID/ANALISIS_02_ HUEZO VAPNIK_INGRID.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#%%
 #1a opción Rutas de importación y exportación
 synergy_dataframe = pd.read_csv('synergy_logistics_database.csv',
                                 index_col=0, encoding='utf-8', parse_dates=[4, 5])
@@ -24,6 +24,7 @@ count_sort = count_sort.to_frame().reset_index()
 #print (count_sort)
 
 #%%
+
 
 #2a opción Medio de transporte utilizado
 synergy_dataframe2 = pd.read_csv('synergy_logistics_database.csv',
@@ -51,11 +52,35 @@ count_sort2=count_sort2.to_frame().reset_index()
 synergy_dataframe3 = pd.read_csv('synergy_logistics_database.csv',
                                 index_col=0, encoding='utf-8', parse_dates=[4, 5])
 
+#Definimos variables de imp. y exp.
+exports=synergy_dataframe3[synergy_dataframe3['direction']=='Exports']
+imports=synergy_dataframe3[synergy_dataframe3['direction']=='Imports']
+
+#Función para obtener el porcentaje acumulado y países pertenecientes
+#al 85%
+def sol_3 (df,p):
+    total_value_origin=df.groupby('origin').sum()['total_value'].reset_index()
+    total_value_percent=total_value_origin['total_value'].sum()    
+    total_value_origin['percent']=100*total_value_origin['total_value']/total_value_percent
+    total_value_origin.sort_values(by='percent',ascending=False,inplace=True)
+    total_value_origin['cumsum']=total_value_origin['percent'].cumsum()
+    lista=total_value_origin[total_value_origin['cumsum']<p]
+    return lista
+
+res_exports=sol_3(exports, 85)
+res_imports=sol_3(imports, 85) 
+
+#Se grafican en un pie chart acorde a su número de índice
+plot=res_exports.plot.pie(y='total_value')
+#print (plot)
+plot2=res_imports.plot.pie(y='total_value')
+#print (plot2)
+
 #Definimos apartados y realizamos sumatoria por país incluyendo
 #imp. y exp.
 combinaciones3=(synergy_dataframe3.groupby(by=['origin'])
                 .sum().groupby(level=[0]).cumsum())
 #print (combinaciones3)
 
-plot=combinaciones3.plot.pie(y='total_value')
-#print (plot)
+plot3=combinaciones3.plot.pie(y='total_value')
+#print (plot3)
